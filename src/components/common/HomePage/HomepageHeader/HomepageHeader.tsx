758988c523c746a878e0da9fa8eac42dd62ab02a
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2024-01-21 23:39:01 +0100

    Adds scroll-to reference on homepage

--- src/components/common/HomePage/HomepageHeader/HomepageHeader.tsx
+++ src/components/common/HomePage/HomepageHeader/HomepageHeader.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
+
+const scrollToDiv = () => {
+  const element = document.querySelector(".newsletter-CTA");
+  element?.scrollIntoView({ behavior: "smooth" });
+};
 
 export const HomepageHeader: React.FC = () => {
-    return (
-        <div className="homepage-header">
-            <div className="column">
-                <div className="content">
-                    <p className="medium-length-hero">Experience the thrill of live football</p>
-                    <p className="lorem-ipsum-dolor">Stay updated with real-time scores and latest headlines</p>
-                </div>
-                <div className="actions">
-                    <div className="style-primary-small">
-                        <button className="button">Explore</button>
-                    </div>
-                </div>
-            </div>
-            <img className="placeholder-image" alt='Placeholder' src="/media/placeholderheader.png"/>
+  return (
+    <div className="homepage-header">
+      <div className="column">
+        <div className="content">
+          <p className="medium-length-hero">
+            Experience the thrill of live football
+          </p>
+          <p className="lorem-ipsum-dolor">
+            Stay updated with real-time scores and latest headlines
+          </p>
         </div>
-    );
+        <div className="actions">
+          <div className="style-primary-small">
+            <button onClick={scrollToDiv} className="button">
+              Explore
+            </button>
+          </div>
+        </div>
+      </div>
+      <img
+        className="placeholder-image"
+        alt="Placeholder"
+        src="/media/placeholderheader.png"
+      />
+    </div>
+  );
 };
