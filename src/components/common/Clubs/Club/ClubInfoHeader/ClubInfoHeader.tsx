2901c04cd263f1712e1654a4f0b3d069fcdf585b
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2024-01-24 21:27:45 +0100

    Populates club and competition information

--- src/components/common/Clubs/Club/ClubInfoHeader/ClubInfoHeader.tsx
+++ src/components/common/Clubs/Club/ClubInfoHeader/ClubInfoHeader.tsx
@@ -1,41 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchClubById } from "../../../../../api/clubs-api"; // Adjust the import path as needed
 import "./style.css";
 
 export const ClubInfoHeader = (): JSX.Element => {
+  const { id: clubId } = useParams();
+
+  const [club, setClub] = useState<{
+    clubId: number;
+    clubCode: string;
+    name: string;
+    domesticCompetitionId: string;
+    squadSize: number;
+    averageAge: number;
+    foreignersNumber: number;
+    foreignersPercentage: number;
+    nationalTeamPlayers: number;
+    stadiumName: string;
+    stadiumSeats: number;
+    netTransferRecord: number;
+    lastSeason: number;
+    url: string;
+  } | null>(null);
+
+  useEffect(() => {
+    const getClub = async () => {
+      try {
+        const clubData = await fetchClubById(clubId || "");
+        setClub(clubData);
+      } catch (error) {
+        console.error("Failed to fetch club:", error);
+      }
+    };
+
+    getClub();
+  }, [clubId]);
+
+  if (!club) {
+    return <div className="club-info-header">Loading...</div>;
+  }
+
   return (
     <div className="club-info-header">
       <div className="content">
-        <div className="user" />
+        <div
+          className="user"
+          style={{
+            backgroundImage: `url(https://tmssl.akamaized.net/images/wappen/head/${club.clubId}.png)`,
+          }}
+        />
         <div className="div">
           <div className="column">
-            <div className="heading">Club Name</div>
-            <p className="text">€ 1.29bn Total Market Value</p>
+            <div className="heading">{club.name}</div>
+            <p className="text">
+              € {club.netTransferRecord} Total Market Value{" "}
+              {/* TODO: Query all players to determine market value */}
+            </p>
           </div>
           <div className="list">
             <div className="content-2">
               <div className="list-item">
                 <div className="text-wrapper">Squad size</div>
-                <div className="text-2">24</div>
+                <div className="text-2">{club.squadSize}</div>
               </div>
               <div className="list-item">
                 <div className="text-wrapper">National team players</div>
-                <div className="text-2">21</div>
+                <div className="text-2">{club.nationalTeamPlayers}</div>
               </div>
             </div>
             <div className="content-2">
               <div className="list-item-2">
                 <div className="text-wrapper">Average age</div>
-                <div className="text-2">27.3</div>
+                <div className="text-2">{club.averageAge}</div>
               </div>
               <div className="list-item-2">
                 <div className="text-wrapper">Stadium</div>
-                <div className="text-3">Etihad Stadium 55017 Seats</div>
+                <div className="text-3">{club.stadiumName}</div>
               </div>
             </div>
             <div className="content-2">
               <div className="list-item-2">
                 <div className="text-wrapper">Foreigners</div>
-                <div className="text-2">17 (70.8%)</div>
+                <div className="text-2">
+                  {club.foreignersNumber} ({club.foreignersPercentage}%)
+                </div>
               </div>
             </div>
           </div>
--- src/components/common/Competitions/Competition/CompetitionInfoHeader/CompetitionInfoHeader.tsx
+++ src/components/common/Competitions/Competition/CompetitionInfoHeader/CompetitionInfoHeader.tsx
@@ -1,41 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  fetchCompetitionById,
+  fetchCompetitonStatistics,
+} from "../../../../../api/competitions-api";
 import "./style.css";
 
 export const CompetitionInfoHeader = (): JSX.Element => {
+  const { id: competitionId } = useParams();
+
+  const [competitionData, setCompetitionData] = useState<{
+    competition: {
+      competitionId: string;
+      competitionCode: string;
+      name: string;
+      subType: string;
+      type: string;
+      countryId: number;
+      countryName: string;
+      domesticLeagueCode: string;
+      confederation: string;
+      url: string;
+    };
+    clubCount: number;
+    totalMarketValue: number;
+    totalNumberOfPlayers: number;
+  } | null>(null);
+
+  const [competitionStats, setCompetitionStats] = useState<{
+    averageMarketValue: number;
+    averageAge: number;
+    percentageForeigners: number;
+  } | null>(null);
+
+  useEffect(() => {
+    const getCompetitionData = async () => {
+      try {
+        const competitionData = await fetchCompetitionById(competitionId || "");
+        setCompetitionData(competitionData);
+
+        const competitionStats = await fetchCompetitonStatistics(
+          competitionId || ""
+        );
+        setCompetitionStats(competitionStats);
+      } catch (error) {
+        console.error("Failed to fetch competition:", error);
+      }
+    };
+    getCompetitionData();
+  }, [competitionId]);
+
+  if (!competitionData) {
+    return <div className="league-info-header">Loading...</div>;
+  }
+
   return (
     <div className="league-info-header">
       <div className="content">
-        <div className="user" />
+        <div
+          className="user"
+          style={{
+            backgroundImage: `url(https://tmssl.akamaized.net/images/logo/header/${competitionData.competition.competitionId.toLowerCase()}.png)`,
+          }}
+        />
         <div className="div">
           <div className="column">
-            <div className="heading">League Name</div>
-            <p className="text">€ 1.29bn Total Market Value</p>
+            <div className="heading">{competitionData.competition.name}</div>
+            <p className="text">
+              € {competitionData.totalMarketValue} Total Market Value
+            </p>
           </div>
           <div className="list">
             <div className="content-2">
               <div className="list-item">
                 <div className="text-wrapper">Teams</div>
-                <div className="text-2">20 teams</div>
+                <div className="text-2">{competitionData.clubCount} teams</div>
               </div>
               <div className="list-item">
                 <div className="text-wrapper">ø-Market value</div>
-                <div className="text-2">£ 19.29m</div>
+                <div className="text-2">
+                  £ {competitionStats?.averageMarketValue}m
+                </div>
               </div>
             </div>
             <div className="content-2">
               <div className="list-item-2">
                 <div className="text-wrapper">Players</div>
-                <div className="text-2">568</div>
+                <div className="text-2">
+                  {competitionData.totalNumberOfPlayers}
+                </div>
               </div>
               <div className="list-item-2">
                 <div className="text-wrapper">ø-Age</div>
-                <div className="text-3">26.5</div>
+                <div className="text-3">{competitionStats?.averageAge}</div>
               </div>
             </div>
             <div className="content-2">
               <div className="list-item-2">
                 <div className="text-wrapper">Foreigners</div>
-                <div className="text-2">387 (68.1 %)</div>
+                <div className="text-2">
+                  ({competitionStats?.percentageForeigners}%)
+                </div>
               </div>
             </div>
           </div>
