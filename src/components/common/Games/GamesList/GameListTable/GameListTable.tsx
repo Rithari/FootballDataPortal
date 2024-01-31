b06bd580d29cd93e1ba8bc5330c7f74880a849bb
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2024-01-31 01:45:19 +0100

    Updates packages, fixes the following: In GamesListTable: - Pagination  now correctly displays, without duplicate pages. This was due to indexing starting at 0. - Fixes Search by actually defining the search interaction with the backend

--- src/components/common/Games/GamesList/GameListTable/GameListTable.tsx
+++ src/components/common/Games/GamesList/GameListTable/GameListTable.tsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 export const GameListTable = (): JSX.Element => {
   const navigate = useNavigate();
 
-  const columns = [
+  const columnNames = [
     {
       name: "gameId",
       hidden: true,
@@ -36,6 +36,39 @@ export const GameListTable = (): JSX.Element => {
     },
   ];
 
+  // Function to build URL with proper query parameters
+  const buildUrl = (
+    prevUrl: any,
+    page: any,
+    limit: any,
+    keyword: any,
+    sort?: any
+  ) => {
+    const url = new URL(prevUrl, window.location.origin);
+    url.searchParams.set("page", page);
+    url.searchParams.set("limit", limit);
+    if (keyword) {
+      url.searchParams.set("searchQuery", keyword);
+    }
+    if (sort && sort.length > 0) {
+      const col = sort[1];
+      const dir = col.direction === 1 ? "asc" : "desc";
+      console.log(col);
+      const columnNames = [
+        null,
+        "home_club_name",
+        "away_club_name",
+        "score",
+        "date",
+        "stadium",
+        "attendance",
+      ];
+      const colName = "home_club_name";
+      url.searchParams.set("sort", `${colName},${dir}`);
+    }
+    return url.toString();
+  };
+
   return (
     <div className="gridjs-container">
       <Grid
@@ -58,15 +91,22 @@ export const GameListTable = (): JSX.Element => {
           },
           total: (responseData: any) => responseData.total,
         }}
-        columns={columns}
-        search={{ enabled: true }}
-        sort={{ enabled: true }}
+        columns={columnNames}
+        search={{
+          enabled: true,
+          server: {
+            url: (prev: any, keyword: any) => buildUrl(prev, 1, 10, keyword),
+          },
+        }}
+        sort={{
+          enabled: false,
+        }}
         pagination={{
           enabled: true,
           limit: 10,
           server: {
             url: (prevUrl: any, page: any, limit: any) =>
-              `${prevUrl}?page=${page}&limit=${limit}`,
+              buildUrl(prevUrl, page, limit, null),
           },
         }}
         className="gridjs-table"
